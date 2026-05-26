918d46b4e102ebda7062a6cd61fdd23eff0f666d
Elijah-Lewis <55295746+Elijah-Lewis@users.noreply.github.com>
2026-05-26 17:35:46 -0400

    initial commit with basic html, css, and js

---
+++ websiteScripts.js
@@ -0,0 +1,14 @@
+function onClick(buttonName){
+    alert(`You clicked the \"${buttonName}\" button!`);
+}
+
+function deletePlantInfo(){
+    alert("You clicked the \"Delete Plant Information\" button!");
+}
+function editPlantInfo(){
+    alert("You clicked the \"Edit Plant Information\" button!");
+}
+
+function addPlantInfo(){
+    alert("You clicked the \"Add Plant Information\" button!");
+}
